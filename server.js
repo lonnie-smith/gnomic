ed6049e6cc2c47177faa63297d81916400cc3f51
Lonnie Smith <lawrence.smith.3@gmail.com>
2019-01-26 09:59:41 -0600

    Adds rudimentary Pug setup for index page

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const port = process.env.PORT || 80;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-
+app.set('view engine', 'pug');
 
 
 const router = express.Router();
@@ -23,6 +22,19 @@ router.get('/fragments', (req, res) => {
 });
 
 app.use('/api', router);
+
+app.get('/', (req, res) => {
+    let fragments;
+    Fragment.list().then(rows => {
+        fragments = rows;
+    })
+    .then(() => {
+        res.render('index', {
+            fragments,
+        });
+    });
+
+});
 
 app.listen(port);
 console.log('Gnomic server started on port', port);
